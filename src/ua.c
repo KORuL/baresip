0179b4891012b8a36344a4e4dc04166142908bd6
Vladimir Orlov <korul1@yandex.ru>
2026-04-01 15:19:09 +0300

    * changes

--- src/ua.c
+++ src/ua.c
@@ -150,8 +150,13 @@ static int create_register_clients(struct ua *ua)
 		err = reg_add(&ua->regl, ua, 0);
 	}
 
-	ua_add_extension(ua, "replaces");
-	ua_add_extension(ua, "norefersub");
+	if (ua->acc->replace) {
+		ua_add_extension(ua, "replaces");
+	}
+	if (ua->acc->norefersub) {
+		ua_add_extension(ua, "norefersub");
+	}
+
 
 	if (ua->acc->rel100_mode)
 		ua_add_extension(ua, "100rel");
